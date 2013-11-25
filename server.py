b66c178230c23d5f36bede35aecb6bd97719c91c
Ben McHone <tycoon177@gmail.com>
2013-11-24 23:32:36 -0600

    Added name changing ability and made sure you cannot change to the same username

--- server.py
+++ server.py
@@ -18,7 +18,10 @@ class ChatServer(object):
         if client_id == 0:
             return "Server"
         else:
-            return "User{0}".format(client_id)
+            try:
+                return self.clients[client_id].name 
+            except:
+                return "User{0}".format(client_id)
 
     def add_client(self, client):
         """
@@ -49,8 +52,6 @@ class ChatServer(object):
         if client_id in self.clients:
             name = self.name(client_id)
             del self.clients[client_id]
-            self.broadcast(0, "{0} has left the chat.".format(self.name(client_id)))
-
 
     def broadcast(self, sender_id, message):
         msg = "{0}: {1}".format(self.name(sender_id), message)
@@ -58,12 +59,21 @@ class ChatServer(object):
             client.write_message(msg)
         print(msg)
 
+    def change_name(self, client_id, new_name):
+        for client in self.clients:
+            if self.name(client) == new_name:
+                self.clients[client_id].write_message("Server: This name is already taken")
+                return None
+        self.broadcast(0, "{0} has been renamed to {1}.".format(self.name(client_id),new_name))
+        self.clients[client_id].name=new_name
+      
 server = ChatServer()
 
 class WSEchoHandler(ws.WebSocketHandler):
     
     def open(self):
         self.client_id = server.add_client(self)
+        self.name="User{0}".format(self.client_id)
         self.time = int(round(time.time()*1000))
         self.spam_msg = int(round(time.time()*1000))
     def on_message(self, message):
@@ -78,6 +88,9 @@ class WSEchoHandler(ws.WebSocketHandler):
                 self.spam_msg=int(round(time.time()*1000))
             return None
         self.time=int(round(time.time()*1000))
+        if message.startswith("/name"):
+            server.change_name(self.client_id, message.split(" ",1)[1])
+            return None
         """
         Safegaurd to disallow xss and html injection.
         """
@@ -85,42 +98,16 @@ class WSEchoHandler(ws.WebSocketHandler):
         message=message.replace('>','&gt;');
         message=message.replace('<','&lt;');
         message=message.replace('"','&quot;');
-        message=message.replace("'","&#x27");
+        message=message.replace("'","&#x27;");
         if(not bool(not message or message.isspace())):
             server.broadcast(self.client_id, message)
 
     def on_close(self):
+        name = self.name
         server.remove_client(self.client_id)
+        server.broadcast(0, "{0} has left the chat".format(self.name))
+       # server.remove_client(self.client_id)
     
-def echo_test_wsgi(env, start):
-    status = "200 OK"
-    response_headers = [("Content-type", "text/html")]
-    start(status, response_headers)
-
-    js = """
-    <html>
-    <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-      <script type="text/javascript">
-        var ws = new WebSocket("ws://%s/ws/echo");
-        ws.onopen = function() {
-            ws.send("Hello, world!");
-        };
-        ws.onmessage = function (evt) {
-            alert(evt.data);
-        };
-      </script>
-    </head>
-    <body>
-      Hopefully, this will call the websocket.
-    </body>
-    </html>
-    """ % (env['HTTP_HOST']) #  env['SERVER_PORT'])
-
-    return js
-
-
-
 application = tornado.web.Application([
     (r'/', WSEchoHandler),
 ])
@@ -128,21 +115,22 @@ application = tornado.web.Application([
 
 def main(cfg):
     """
-    A tornado application that supports serving both http and websockets.
+    A tornado application that supports serving websockets.
     cfg should be a dict-like thing with 'host' and 'port' entries.
     """
     app = tornado.web.Application([
             (r'/ws/echo', WSEchoHandler),
-            (r'/echo-test', tornado.web.FallbackHandler, {
-                    'fallback': tornado.wsgi.WSGIContainer(echo_test_wsgi) }),
-            ], debug=True)
+            ], debug=(cfg['debug']=="true"))
 
     server = tornado.httpserver.HTTPServer(app)
     server.listen(port=cfg['port'], address=cfg['host'])
-
+    on = "on"
+    if not cfg['debug']=="true":
+        on = "off"
     print("starting server at http://{host}:{port}/".format(**cfg))
+    print("Debug mode {0}".format(on))
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-    cfg = { "host": "0.0.0.0", "port" : "8888" }
+    cfg = { "host": "0.0.0.0", "port" : "8888","debug": "true" }
     main(cfg)
